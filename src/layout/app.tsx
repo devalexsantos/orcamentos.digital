23371b0ea290037ace098036e3415b76c30e6a00
Alex Santos <devalexsantos@gmail.com>
2024-02-14 15:09:26 -0300

    chore: remove clerk authenticate

--- src/layout/app.tsx
+++ src/layout/app.tsx
@@ -1,32 +1,11 @@
 import { SearchBar } from '@/components/search-bar'
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-} from '@clerk/clerk-react'
 import { Outlet } from 'react-router-dom'
 
 export default function AppLayout() {
-  const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
-
-  if (!PUBLISHABLE_KEY) {
-    throw new Error('Missing Publishable Key')
-  }
-
   return (
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <div className="container">
-        <SignedOut>
-          <SignInButton />
-        </SignedOut>
-      </div>
-      <SignedIn>
-        <div className="container p-8 flex flex-col gap-4 max-w-4xl">
-          <SearchBar />
-          <Outlet />
-        </div>
-      </SignedIn>
-    </ClerkProvider>
+    <div className="container p-8 flex flex-col gap-4 max-w-4xl">
+      <SearchBar />
+      <Outlet />
+    </div>
   )
 }
