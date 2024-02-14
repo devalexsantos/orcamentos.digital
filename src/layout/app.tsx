a244877089c5ea7724ab29267eaf68548593f82e
Alex Santos <devalexsantos@gmail.com>
2024-02-14 15:38:05 -0300

    BREAKCHANGE: adicioned firebase auth

--- src/layout/app.tsx
+++ src/layout/app.tsx
@@ -1,9 +1,49 @@
 import { SearchBar } from '@/components/search-bar'
-import { Outlet } from 'react-router-dom'
+import { useNavigate, Outlet } from 'react-router-dom'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signOut,
+} from 'firebase/auth'
+import { app } from '@/services/firebase'
 
 export default function AppLayout() {
+  const navigate = useNavigate()
+
+  const auth = getAuth(app)
+  const [user, loading] = useAuthState(auth)
+
+  const killSessionFirebase = () => {
+    signOut(auth)
+      .then(() => {
+        navigate('/login')
+      })
+      .catch((error) => console.log(error.message))
+  }
+
+  if (loading) return <div>Loading...</div>
+
+  if (!user) {
+    return (
+      <div>
+        <h1>Not logged in</h1>
+        <button
+          onClick={() => {
+            const provider = new GoogleAuthProvider()
+            signInWithPopup(auth, provider)
+          }}
+        >
+          Sign in with Google
+        </button>
+      </div>
+    )
+  }
+
   return (
     <div className="container p-8 flex flex-col gap-4 max-w-4xl">
+      <button onClick={() => killSessionFirebase()}>Sair</button>
       <SearchBar />
       <Outlet />
     </div>
